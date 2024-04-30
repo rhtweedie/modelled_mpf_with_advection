197b0f58b6e5a57e8f71b2647312584e8a809a2b
Heather Tweedie <zcfbrht@ucl.ac.uk>
2024-04-30 21:30:46 +0100

    Add script to calculate mean of forwarded SIR

---
+++ calc_mean_sir.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+    YEAR = 2017
+    SPACING = 8
+    DAYS_TO_FORWARD = 183
+
+    sir_advected = pd.read_pickle(f'/home/htweedie/melt_ponds/data/forwarded_mpfs/testing/sir_from_{YEAR}0401_{DAYS_TO_FORWARD}_days_spacing_{SPACING}.pkl')
+
+    date_from = format_date(YEAR, '04', '01')
+    date_to = format_date(YEAR, '08', '31')
+
+    subset_sir = sir_advected.loc[date_from:date_to]
+    num_points = subset_sir.shape[1]
+
+    mean_sir = np.zeros(num_points)
+    for i in range(num_points):
+        if i % 10000 == 0:
+            print(f'Processing grid cell {i} of {num_points}')
+        timeseries = np.asarray(subset_sir[i])
+        mean_sir[i] = np.nanmean(timeseries)
+
+    fn = f'/home/htweedie/melt_ponds/data/forwarded_mpfs/testing/mean_summer_sir_{YEAR}'
+    np.save(fn, mean_sir)
+    print(f'Data saved at {fn}')
+
+
+
+def format_date(year, month, day):
+    return f"{year}-{month}-{day} 12:00:00"
+
+
+
+if __name__ == "__main__":
+    main()
